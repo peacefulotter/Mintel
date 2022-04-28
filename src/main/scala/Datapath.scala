ed08260d0aaead1c3b66528b3c8be743222417a7
Gahm8684 <gabrielhernandez@ufl.edu>
2022-04-28 12:27:36 -0400

    committing updates to datapath, ALUTest, and decoder

--- src/main/scala/Datapath.scala
+++ src/main/scala/Datapath.scala
@@ -10,8 +10,30 @@ class Datapath extends Module {
     val writeback = Module( new Writeback )
 
     val io = IO( new Bundle {
-        val instr = Output(UInt(32.W))
+        val switches1 = Input(UInt(8.W)) // Input 1
+        val switches2 = Input(UInt(8.W)) // Input 2
+
+        val instr     = Output(UInt(32.W))
+
+        // Input 1
+        val disp0     = Output(UInt(6.W))
+        val disp1     = Output(UInt(6.W))
+
+        // Input 2
+        val disp2     = Output(UInt(6.W))
+        val disp3     = Output(UInt(6.W))
+
+        // Output
+        val disp4     = Output(UInt(6.W))
+        val disp5     = Output(UInt(6.W))
+        val disp6     = Output(UInt(6.W))
+        val disp7     = Output(UInt(6.W))
     } )
+
+    val input1 = io.switches1
+    val input2 = io.switches2
+    val output = WireDefault(0.U(32.W))
+    output := execute.io.AluRes
 
     io.instr := fetch.io.Instr;
 
@@ -65,4 +87,36 @@ class Datapath extends Module {
     writeback.io.ReadData := RegNext(memory.io.ReadData)
     writeback.io.AddrData := RegNext(memory.io.AluResOut)
     writeback.io.WriteRegAddrIn := RegNext(memory.io.WriteRegAddrOut)
+
+    /** 7-Seg Displays **/
+    val U_decoder7seg_0 = Module( new decoder7seg )
+    val U_decoder7seg_1 = Module( new decoder7seg )
+    val U_decoder7seg_2 = Module( new decoder7seg )
+    val U_decoder7seg_3 = Module( new decoder7seg )
+    val U_decoder7seg_4 = Module( new decoder7seg )
+    val U_decoder7seg_5 = Module( new decoder7seg )
+    val U_decoder7seg_6 = Module( new decoder7seg )
+    val U_decoder7seg_7 = Module( new decoder7seg )
+
+    U_decoder7seg_0.io.in := input1(3,0)
+    U_decoder7seg_1.io.in := input1(7,4)
+    U_decoder7seg_2.io.in := input2(3,0)
+    U_decoder7seg_3.io.in := input2(7,4)
+
+    U_decoder7seg_4.io.in := output(3,0)
+    U_decoder7seg_5.io.in := output(7,4)
+    U_decoder7seg_6.io.in := output(11,8)
+    U_decoder7seg_7.io.in := output(15,12)
+
+    io.disp0 := U_decoder7seg_0.io.out // Input1 -> Switches(3:0)
+    io.disp1 := U_decoder7seg_1.io.out // Input1 -> Switches(7:4)
+    io.disp2 := U_decoder7seg_2.io.out // Input1 -> Switches(3:0)
+    io.disp3 := U_decoder7seg_3.io.out // Input1 -> Switches(7:4)
+    io.disp4 := U_decoder7seg_4.io.out // Output -> Output(3:0)
+    io.disp5 := U_decoder7seg_5.io.out // Output -> Output(7:4)
+    io.disp6 := U_decoder7seg_6.io.out // Output -> Output(11:8)
+    io.disp7 := U_decoder7seg_7.io.out // Output -> Output(15:12)
+
+
+
 }
--- src/test/scala/ALUTest.scala
+++ src/test/scala/ALUTest.scala
@@ -3,75 +3,72 @@ import chiseltest._
 import org.scalatest.flatspec.AnyFlatSpec
 
 class ALUTest extends AnyFlatSpec with ChiselScalatestTester {
-  behavior of "ALUTest"
-  it should "pass" in {
-    test(new ALU).withAnnotations(Seq(WriteVcdAnnotation)) { alu =>
+  "ALU" should "pass" in {
+
+    test(new ALU) { dut =>
       var a = 0
       var b = 0
 
-      def bubl(): Int = {
-        0
-      }
+      def add(a: Int, b: Int): Int   = {a + b}
+      def sub(a: Int, b: Int): Int   = {a - b}
+      def and(a: Int, b: Int): Int   = {a & b}
+      def or (a: Int, b: Int): Int   = {a | b}
+      def xor(a: Int, b: Int): Int   = {a ^ b}
 
-      def pass(a: Int, b: Int): Int = {
-        a
-      }
-
-      def inc(a: Int, b: Int): Int = {
-        a + 1
-      }
-
-      def dec(a: Int, b: Int): Int = {
-        a - 1
-      }
-
-      def add(a: Int, b: Int): Int = {
-        a + b
-      }
-
-      def sub(a: Int, b: Int): Int = {
-        a - b
-      }
-
-      def not(a: Int, b: Int): Int = {
-        ~a
-      }
-
-      def and(a: Int, b: Int): Int = {
-        a & b
-      }
-
-      def or(a: Int, b: Int): Int = {
-        a | b
-      }
-
-      def xor(a: Int, b: Int): Int = {
-        a ^ b
-      }
+      def sll(a: Int, b: Int): Int   = {a << b}
+      def srl(a: Int, b: Int): Int   = {a >> b}
+      //def slt(a: Int, b: Int): Int   = {(a.asSInt < b.asSInt).litValue.toInt}
 
       for (j <- 0 until 10) {
 
         a = j
-        alu.io.A.poke(a.U)
+        dut.io.A.poke(a.U)
+
+        println("A = " + a.toString)
 
         for (k <- 0 until 10) {
 
           b = k
-          alu.io.B.poke(b.U)
-          val functions = Vector(bubl(), pass(a, b), inc(a, b), dec(a, b), add(a, b), sub(a, b), not(a, b), and(a, b), or(a, b), xor(a, b))
+          dut.io.B.poke(b.U)
 
-          for (i <- 0 until 10) {
+          println("B = " + b.toString)
 
-            alu.io.alu_op.poke(i.U)
+          val functions = Vector(add(a,b), sub(a,b), and(a,b), or(a,b), xor(a,b), sll(a,b), srl(a,b))
+          //al functions = Vector(add(a,b), sub(a,b), and(a,b), or(a,b), xor(a,b))
 
-            if (functions(index = i) >= 0) { // ALU does not support negative numbers
-              //alu.io.out.expect(functions(index = i).U)
+          for (i <- 0 until functions.length) {
+
+            println("AluOp = " + i.toString)
+            dut.io.AluOp.poke(i.U)
+
+            println("Result should be = " + functions(i).toString)
+            println("Result is = " + dut.io.out.peek().litValue)
+
+            if(functions(i) >= 0){    // ALU does not support negative numbers
+              dut.io.out.expect(functions(i).U)
             }
-            alu.clock.step(1)
+            dut.clock.step()
 
           }
         }
       }
+
+
+//      dut.io.A.poke(1.U)
+//      dut.io.B.poke(2.U)
+//      dut.io.AluOp.poke(ALU.add)
+//      dut.clock.step()
+//
+//      dut.io.out.expect(3.U)
+//      println("Addition passes since " + dut.io.A.toString() + "+" + dut.io.B + " is indeed ")
+//
+//      dut.io.A.poke(1.U)
+//      dut.io.B.poke(2.U)
+//      dut.io.AluOp.poke(ALU.add)
+//      dut.clock.step()
+//
+//      dut.io.out.expect(3.U)
+//      println("")
     }
   }
 }
